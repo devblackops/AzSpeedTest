083f4de26c2fa564f2e4d1c175549f44bcaaef87
Daniel T. Thorpe <daniel.thorpe@citrix.com>
2019-08-20 14:29:36 +0100

    fix(azspeedtest.psm1): change case of Public / Private
    this makes the code work correctly on *nix platforms, as the folders are created capitalised, so
    will not import natively

--- AzSpeedTest/AzSpeedTest.psm1
+++ AzSpeedTest/AzSpeedTest.psm1
@@ -1,12 +1,12 @@
 
 #Set-StrictMode -Version 2.0
 
-$script:storageLocations = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath 'private/storageLocations.json') -Raw |
+$script:storageLocations = Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/storageLocations.json') -Raw |
     ConvertFrom-Json
 
 # Dot source public/private functions
-$public =  @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'public/*.ps1') -Recurse -ErrorAction Stop)
-$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'private/*.ps1') -Recurse -ErrorAction Stop)
+$public =  @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1') -Recurse -ErrorAction Stop)
+$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
 foreach ($import in @($public + $private)) {
     try {
         . $import.FullName
